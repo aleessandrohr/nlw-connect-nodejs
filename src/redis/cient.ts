02a9bbb9e56e920f7bd6bdf9e53b0f17e1f4c46e
Alessandro Henrique Ramos <alessandro.henri.r@gmail.com>
2025-03-02 11:44:47 -0300

    change max retries per request in redis

--- src/redis/cient.ts
+++ src/redis/cient.ts
@@ -1,6 +1,4 @@
 import { env } from '@/env'
 import { Redis } from 'ioredis'
 
-export const redis = new Redis(env.REDIS_URL, {
-  maxRetriesPerRequest: null,
-})
+export const redis = new Redis(env.REDIS_URL)
