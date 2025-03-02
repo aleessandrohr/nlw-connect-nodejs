9acb3bd50ad1624b77886ad413be7577a168087d
Alessandro Henrique Ramos <alessandro.henri.r@gmail.com>
2025-03-02 11:09:01 -0300

    test

--- src/redis/cient.ts
+++ src/redis/cient.ts
@@ -1,4 +1,4 @@
 import { env } from '@/env'
 import { Redis } from 'ioredis'
 
-export const redis = new Redis(env.REDIS_URL, { maxRetriesPerRequest: 50 })
+export const redis = new Redis(env.REDIS_URL)
